915b0ca5a42776e7f7b5f0bae66fa58b050d93c8
Sean Hubbard <sean.hubbard@trilogy.com>
2026-01-29 09:46:24 -0500

    Update campuses.js

--- src/data/campuses.js
+++ src/data/campuses.js
@@ -3,7 +3,7 @@ export const CAMPUSES = [
   { name: "Alpha High School", state: "Texas", city: "Austin", cc: "Ian Huggins", ccEmail: "ian.huggins@alpha.school", ccPhone: "(971) 222-9736", owner: "Aaron Crute", ownerEmail: "aaron.crute@trilogy.com", ownerPhone: "(404) 449-5043" },
   { name: "Alpha School Miami", state: "Florida", city: "Miami", cc: "Maria Mejia", ccEmail: "maria.mejia@alpha.school", ccPhone: "(305) 775-3375", owner: "Tara Stephenson", ownerEmail: "tara.stephenson@trilogy.com", ownerPhone: "(704) 974-1413" },
   { name: "Alpha School Brownsville", state: "Texas", city: "Brownsville", cc: "Kathrine Ledesma", ccEmail: "kathrine.ledesma@alpha.school", ccPhone: "(956) 230-6648", owner: "Randall Grant", ownerEmail: "randall.grant@trilogy.com", ownerPhone: "(850) 212-6957" },
-  { name: "Texas Sports Academy", state: "Texas", city: "Austin", cc: "Cyndie Jensen", ccEmail: "cyndie.jensen@sportsacademy.school", ccPhone: "", owner: "Aaron Crute", ownerEmail: "aaron.crute@trilogy.com", ownerPhone: "(404) 449-5043" },
+  { name: "Texas Sports Academy - Lake Travis", state: "Texas", city: "Austin", cc: "Cyndie Jensen", ccEmail: "cyndie.jensen@sportsacademy.school", ccPhone: "", owner: "Aaron Crute", ownerEmail: "aaron.crute@trilogy.com", ownerPhone: "(404) 449-5043" },
   { name: "GT School", state: "Texas", city: "Georgetown", cc: "Keith Wong", ccEmail: "keith.wong@2hourlearning.com", ccPhone: "", owner: "Tara Stephenson", ownerEmail: "tara.stephenson@trilogy.com", ownerPhone: "(704) 974-1413" },
   { name: "Nova Academy Austin", state: "Texas", city: "Austin", cc: "Hiring CC", ccEmail: "", ccPhone: "", owner: "Randall Grant", ownerEmail: "randall.grant@trilogy.com", ownerPhone: "(850) 212-6957" },
   { name: "Nova Academy Bastrop", state: "Texas", city: "Bastrop", cc: "Not Yet", ccEmail: "", ccPhone: "", owner: "Randall Grant", ownerEmail: "randall.grant@trilogy.com", ownerPhone: "(850) 212-6957" },
@@ -21,10 +21,10 @@ export const CAMPUSES = [
   { name: "Alpha School Lake Forest", state: "California", city: "Lake Forest", cc: "Hiring CC", ccEmail: "", ccPhone: "", owner: "Jake Petersen", ownerEmail: "jake.petersen@trilogy.com", ownerPhone: "(712) 309-1377" },
   { name: "Alpha School Raleigh", state: "North Carolina", city: "Raleigh", cc: "Hiring CC", ccEmail: "", ccPhone: "", owner: "Jake Petersen", ownerEmail: "jake.petersen@trilogy.com", ownerPhone: "(712) 309-1377" },
   { name: "Alpha School Houston", state: "Texas", city: "Houston", cc: "Not Yet", ccEmail: "", ccPhone: "", owner: "Randall Grant", ownerEmail: "randall.grant@trilogy.com", ownerPhone: "(850) 212-6957" },
-  { name: "Alpha School Tampa", state: "Florida", city: "Tampa", cc: "Not Yet", ccEmail: "", ccPhone: "", owner: "JR Houston", ownerEmail: "jr.houston@trilogy.com", ownerPhone: "(321) 890-5650" },
-  { name: "Alpha School Orlando", state: "Florida", city: "Orlando", cc: "Not Yet", ccEmail: "", ccPhone: "", owner: "JR Houston", ownerEmail: "jr.houston@trilogy.com", ownerPhone: "(321) 890-5650" },
   { name: "Montessorium Cedar Park", state: "Texas", city: "Cedar Park", cc: "Hiring CC", ccEmail: "", ccPhone: "", owner: "Randall Grant", ownerEmail: "randall.grant@trilogy.com", ownerPhone: "(850) 212-6957" },
-  { name: "Carrollton", state: "Texas", city: "Carrollton", cc: "Sara Leightman", ccEmail: "sara.leightman@sportsacademy.school", ccPhone: "(214) 392-8218", owner: "Kevin Kongslie", ownerEmail: "kevin.kongslie@trilogy.com", ownerPhone: "(609) 933-6838" }
+  { name: "Texas Sports Academy - Carrollton", state: "Texas", city: "Carrollton", cc: "Sara Leightman", ccEmail: "sara.leightman@sportsacademy.school", ccPhone: "(214) 392-8218", owner: "Kevin Kongslie", ownerEmail: "kevin.kongslie@trilogy.com", ownerPhone: "(609) 933-6838" },
+  { name: "Cross-Campus", state: "", city: "", cc: "", ccEmail: "", ccPhone: "", owner: "", ownerEmail: "", ownerPhone: "" },
+  { name: "TESTING - Copy of Ops P2 Template", state: "", city: "", cc: "", ccEmail: "", ccPhone: "", owner: "", ownerEmail: "", ownerPhone: "" }
 ];
 
 export const getCampusesByState = () => {
